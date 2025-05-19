dc8a5243bc876d4255f09d57ad2d7fabc8448e16
Eli <eli.serra173@gmail.com>
2025-05-19 18:38:43 +0200

    Fix import path for schemas in device_helper module

--- hw_proxy/hw_proxy/tools/device_helper.py
+++ hw_proxy/hw_proxy/tools/device_helper.py
@@ -2,7 +2,7 @@
 Device helper for hw_proxy module
 """
 from typing import Optional
-from hw_proxy.schemas import DeviceConfigSchemas, PrinteImageConfSchemas
+from hw_proxy.hw_proxy.core.schemas import DeviceConfigSchemas, PrinteImageConfSchemas
 from hw_proxy.supported_devices import device_list
 
 
