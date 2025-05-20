77ecc440c5ff5cc7185d3b68209bd994ea968119
Eli Serra <eli.serra173@gmail.com>
2025-05-20 19:47:01 +0200

    Refactor hw_proxy routes to enhance logging and error handling

--- hw_proxy/hw_proxy/app/routes/hw_proxy.py
+++ hw_proxy/hw_proxy/app/routes/hw_proxy.py
@@ -1,14 +1,19 @@
 """
 Api routes for hw_proxy module
 """
+import logging
 import asyncio
 import base64
 from fastapi import APIRouter, HTTPException, Header, Request
 from fastapi.responses import JSONResponse
 from escpos.printer import Dummy
+from hw_proxy.__init__ import configure_logging
 from hw_proxy.tools.pos_helper import EscPosHelper
 from hw_proxy.core.schemas import PrintRequest
 
+logging.basicConfig()
+configure_logging(debug=True)
+logger = logging.getLogger("hw_proxy")
 
 router = APIRouter(prefix="/hw_proxy", tags=["hw_proxy"])
 
@@ -21,9 +26,17 @@ CMD_CASHDRAWER = b"\x1B\x70\x00\x19\xFA"  # Apertura de cajón
 
 # --- Security Dependency ---
 # , dependencies=[Depends(verify_secret)]
-async def verify_secret(x_iot_box_secret: str = Header(..., alias="X-IOT-BOX-SECRET")):
+async def verify_secret(
+    x_iot_box_secret: str = Header(
+        ...,
+        alias="X-IOT-BOX-SECRET"
+    )
+):
     if x_iot_box_secret != IOT_BOX_SECRET:
-        raise HTTPException(status_code=403, detail="Forbidden: invalid IoT Box secret")
+        raise HTTPException(
+            status_code=403,
+            detail="Forbidden: invalid IoT Box secret"
+        )
     return True
 
 
@@ -50,13 +63,14 @@ async def handshake(body: dict):
         }
     }
 
+
 @router.post("/customer_facing_display")
 async def customer_facing_display(req: Request):
     body = await req.json()
     req_id = body.get("id")
     params = body.get("params", {})
     # Fake action: log or process display data
-    print("[DISPLAY]", params)
+    logger.debug("[DISPLAY]", params)
     return {"jsonrpc": "2.0", "id": req_id, "result": {"success": True}}
 
 
@@ -64,7 +78,7 @@ async def customer_facing_display(req: Request):
 async def status_json(req: Request):
     body = await req.json()
     req_id = body.get("id")
-    
+
     def _query_status():
         try:
             pos = EscPosHelper(PRINTER_KEY)
@@ -94,39 +108,55 @@ async def status_json(req: Request):
 @router.post("/default_printer_action")
 async def default_printer_action(req: Request):
     """Handle generic default_printer_action calls from Odoo POS"""
-    print("Get data...")
+    logger.debug("Start Default printer Action...")
     body = await req.json()
     req_id = body.get("id")
     params = body.get("params", {})
     data = params.get("data", {})
     action = data.get("action")
     receipt = data.get("receipt")
-    def _job():
-        pos = EscPosHelper(PRINTER_KEY)
-        printer = pos.get_printer()
-        # Choose action
-        if action == "print_receipt":
-            if receipt is not None:
-                print("Convert to Image...")
-                img = pos.format_base64_to_image(receipt)
-                d = Dummy()
-                # debug_save_image(img)
-                #eimg = EscposImage(img_source=f"/tmp/ticket.png")
-                #raster = eimg.to_raster_format()
-                d.image(img, impl="bitImageColumn")
-                d.cut(feed=True)
-                printer._raw(d.output)
-                print("Ticket imprimido...")
-        elif action == "cut":
-            printer._raw(CMD_CUT)
-        elif action in ("cashbox", "cashdrawer"):  # Odoo may use 'cashbox'
-            printer._raw(CMD_CASHDRAWER)
-        else:
-            # unknown action, ignore or log
-            print(f"[WARNING] Unknown default_printer_action: {action}")
-        printer.close()
+    try:
+        def _job():
+            pos = EscPosHelper(PRINTER_KEY)
+            printer = pos.get_printer()
+            # Choose action
+            if action == "print_receipt":
+                if receipt is not None:
+                    logger.debug("Convert to Image...")
+                    img = pos.format_base64_to_image(receipt)
+                    d = Dummy()
+                    # debug_save_image(img)
+                    # eimg = EscposImage(img_source=f"/tmp/ticket.png")
+                    # raster = eimg.to_raster_format()
+                    d.image(img, impl="bitImageColumn")
+                    d.cut(feed=True)
+                    printer._raw(d.output)
+                    logger.debug("Ticket imprimido...")
+            elif action == "cut":
+                printer._raw(CMD_CUT)
+            # Odoo may use 'cashbox'
+            elif action in ("cashbox", "cashdrawer"):
+                printer._raw(CMD_CASHDRAWER)
+            else:
+                # unknown action, ignore or log
+                logger.warning(
+                    "[WARNING] Unknown default_printer_action: "
+                    f"{action}"
+                )
+            printer.close()
+    except Exception as e:
+        logger.error(
+            f"Fatal Error: Unable to run printer action : {action}"
+        )
+        logger.debug(
+            f"exception : {e}"
+        )
+        return HTTPException(
+            status_code=400,
+            detail=f"Fatal Error: Unable to run printer action : {action}"
+        )
     # asyncio.create_task(asyncio.to_thread(_job))
-    print("Start job...")
+    logger.debug("Start job...")
     _job()
     return {"jsonrpc": "2.0", "id": req_id, "result": {"success": True}}
 
@@ -137,7 +167,10 @@ async def print_ticket(req: PrintRequest):
     try:
         data_bytes = base64.b64decode(req.data)
     except Exception as e:
-        raise HTTPException(status_code=400, detail=f"Invalid base64 data: {e}")
+        raise HTTPException(
+            status_code=400,
+            detail=f"Invalid base64 data: {e}"
+        )
 
     async def _job():
         pos = EscPosHelper(PRINTER_KEY)
@@ -151,6 +184,7 @@ async def print_ticket(req: PrintRequest):
     asyncio.create_task(_job())
     return {"success": True}
 
+
 @router.post("/open_cashdrawer")
 async def open_cashdrawer(printer_name: str):
     """Open cash drawer without printing."""
@@ -160,6 +194,7 @@ async def open_cashdrawer(printer_name: str):
         await asyncio.to_thread(printer._raw, CMD_CASHDRAWER)
     asyncio.create_task(_job())
     return {"success": True}
+
 
 @router.post("/cut")
 async def cut_paper(printer_name: str):
